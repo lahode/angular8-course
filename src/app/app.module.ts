4b6116dcbf24f1d03acafe228960ef6ffdfb93cd
astrobob <lahode@infomaniak.ch>
2019-11-08 16:09:21 +0100

    Transform to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroupListComponent } from './group-list/group-list.component';
+import { PagerComponent } from './pager/pager.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupListComponent,
+    PagerComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
